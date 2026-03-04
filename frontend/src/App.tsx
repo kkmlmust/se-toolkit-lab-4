9ce062962dca994c077f5ad485c90a89cba58b65
Kamil Mustafin <k.mustafin@innopolis.unversity>
2026-03-04 20:26:38 +0300

    feat: add description column to the front-end table

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,6 +7,7 @@ interface Item {
   id: number
   type: string
   title: string
+  description: string
   created_at: string
 }
 
@@ -122,6 +123,7 @@ function App() {
                   <td>{item.id}</td>
                   <td>{item.type}</td>
                   <td>{item.title}</td>
+                  <td>{item.description}</td>
                   <td>{item.created_at}</td>
                 </tr>
               ))}
